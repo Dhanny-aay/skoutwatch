f6cf2cc19740264f027c80eafde476a50dce10c6
Dhanny-aay <danielakintolu@gmail.com>
2024-10-03 13:31:35 +0100

    text sizes for result pages

--- src/dashboard/pages/result.js
+++ src/dashboard/pages/result.js
@@ -15,15 +15,15 @@ const Result = () => {
               Back
             </p>
           </span> */}
-          <p className=" font-LatoNormal font-semibold text-[#231546] text-2xl">
+          <p className=" font-LatoNormal font-semibold text-[#231546] text-3xl">
             Data Analysis
           </p>
         </div>
 
-        <p className=" font-Inter font-semibold text-base text-black mt-10">
+        <p className=" font-Inter font-semibold text-lg text-black mt-10">
           Object tracking
         </p>
-        <p className=" text-xs text-[#000000B2] font-normal font-Inter mt-2">
+        <p className=" text-sm text-[#000000B2] font-normal font-Inter mt-2">
           You'll be able to see the results from our AI, segmenting and tracking
           objects and applying visual effects to your video here.
         </p>
@@ -31,7 +31,7 @@ const Result = () => {
         <div className=" w-full grid grid-cols-1 lg:grid-cols-2 gap-6 mt-6">
           {/* input video */}
           <div className=" w-full">
-            <p className=" font-semibold font-Inter text-base text-black">
+            <p className=" font-semibold font-Inter text-xl text-black">
               Input Video
             </p>
 
@@ -45,8 +45,8 @@ const Result = () => {
           </div>
           {/* Output video */}
           <div className=" w-full">
-            <p className=" font-semibold font-Inter text-base text-black">
-              Output
+            <p className=" font-semibold font-Inter text-xl text-black">
+              Output Video
             </p>
             <video className=" rounded-[10px] w-full mt-2" controls>
               <source src={outAna} type="video/webm" />
--- src/dashboard/pages/resultSeg.js
+++ src/dashboard/pages/resultSeg.js
@@ -13,15 +13,15 @@ const ResultSeg = () => {
               Back
             </p>
           </span> */}
-          <p className=" font-LatoNormal font-semibold text-[#231546] text-2xl">
+          <p className=" font-LatoNormal font-semibold text-[#231546] text-3xl">
             Segmentation
           </p>
         </div>
 
-        <p className=" font-Inter font-semibold text-base text-black mt-10">
+        <p className=" font-Inter font-semibold text-lg text-black mt-10">
           Object tracking
         </p>
-        <p className=" text-xs text-[#000000B2] font-normal font-Inter mt-2">
+        <p className=" text-sm text-[#000000B2] font-normal font-Inter mt-2">
           You'll be able to see the results from our AI, segmenting and tracking
           objects and applying visual effects to your video here.
         </p>
@@ -29,7 +29,7 @@ const ResultSeg = () => {
         <div className=" w-full grid grid-cols-1 lg:grid-cols-2 gap-6 mt-6">
           {/* input video */}
           <div className=" w-full">
-            <p className=" font-semibold font-Inter text-base text-black">
+            <p className=" font-semibold font-Inter text-xl text-black">
               Input Video
             </p>
 
@@ -43,8 +43,8 @@ const ResultSeg = () => {
           </div>
           {/* Output video */}
           <div className=" w-full">
-            <p className=" font-semibold font-Inter text-base text-black">
-              Output
+            <p className=" font-semibold font-Inter text-xl text-black">
+              Output Video
             </p>
             {/* <video className=" rounded-[10px] w-full mt-2" controls>
               <source src={outAna} type="video/webm" />
