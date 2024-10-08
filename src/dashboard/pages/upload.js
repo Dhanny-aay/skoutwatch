87af5a80c6adf43e20421459c4ace7da811cbcf3
Dhanny-aay <danielakintolu@gmail.com>
2024-10-08 13:09:52 +0100

    update upload screen

--- src/dashboard/pages/upload.js
+++ src/dashboard/pages/upload.js
@@ -7,8 +7,12 @@ const Upload = () => {
       <Navbar />
       <div className=" flex items-center justify-center py-6 h-[85vh] mt-20">
         <div className=" px-6 py-4 w-[500px] h-full relative">
-          <p className=" text-[#353535] font-LatoNormal font-semibold text-base">
+          <p className=" text-[#353535] font-LatoNormal font-semibold text-xl">
             Upload
+          </p>
+          <p className="text-[#353535] font-Inter font-normal text-xs mt-2">
+            Make sure the video you select includes the object you want to
+            segment in the first frame.
           </p>
           <VideoUpload />
         </div>
