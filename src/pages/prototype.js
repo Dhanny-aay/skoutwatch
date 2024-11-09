ad59002a203719d41491ee094b4e097e52251fc6
Dhanny-aay <danielakintolu@gmail.com>
2024-11-09 21:21:02 +0100

    update

--- src/pages/prototype.js
+++ src/pages/prototype.js
@@ -7,7 +7,7 @@ import load from "./assets/blackload.gif";
 const Prototype = () => {
   const [videoSrc, setVideoSrc] = useState(null);
   const [videoThumbnail, setVideoThumbnail] = useState(null);
-  const [segmentationResponse, setSegmentationResponse] = useState(null); // SAM-2 API response
+  const [segmentationResponse, setSegmentationResponse] = useState(null); // SAM-2 API data response
   const [getRequestData, setGetRequestData] = useState(null); // Store the GET request response
   const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
   const UNDERDOG_BASE_URL = process.env.REACT_APP_UNDERDOG_BASE_URL;
